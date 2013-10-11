efa7054fac5194002d4f62af8ad76b4845fdef81
Sho Nakatani <lay.sakura@gmail.com>
2013-10-11 09:46:44 +0900

    initial commit Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Search::Fulltext::Tokenizer::MeCab - It's new $module
+
+# SYNOPSIS
+
+    use Search::Fulltext::Tokenizer::MeCab;
+
+# DESCRIPTION
+
+Search::Fulltext::Tokenizer::MeCab is ...
+
+# LICENSE
+
+Copyright (C) Sho Nakatani.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Sho Nakatani <lay.sakura@gmail.com>
---
+++ lib/Search/Fulltext/Tokenizer/MeCab.pm
@@ -0,0 +1,39 @@
+package Search::Fulltext::Tokenizer::MeCab;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Search::Fulltext::Tokenizer::MeCab - It's new $module
+
+=head1 SYNOPSIS
+
+    use Search::Fulltext::Tokenizer::MeCab;
+
+=head1 DESCRIPTION
+
+Search::Fulltext::Tokenizer::MeCab is ...
+
+=head1 LICENSE
+
+Copyright (C) Sho Nakatani.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Sho Nakatani E<lt>lay.sakura@gmail.comE<gt>
+
+=cut
+
